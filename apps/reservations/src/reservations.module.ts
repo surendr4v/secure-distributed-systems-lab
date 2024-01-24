13acb9f13f5db598da1f541e8d3c148b6ef1b937
surendra4v <surendra4v@gmail.com>
2024-01-23 19:50:44 -0600

    reservation crud

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
+import { DatabaseModule } from '@app/common/database/database.module';
+import { ReservationsModule } from './reservations/reservations.module';
+
+@Module({
+  imports: [DatabaseModule, ReservationsModule],
+  controllers: [ReservationsController],
+  providers: [ReservationsService],
+})
+export class ReservationsModule {}
